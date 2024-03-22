48a6267c4c4f98079f669f01817374191bd6c468
Davide Pedrotti <unibox@duck.com>
2024-03-22 18:40:13 +0100

    extracted index.html from src folder

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import vitePugPlugin from 'vite-plugin-pug-transformer';
 
 export default defineConfig({
   plugins: [vitePugPlugin()],
-  root: 'src',
   build: {
-    outDir: '../dist',
+    outDir: 'dist',
   },
 });
